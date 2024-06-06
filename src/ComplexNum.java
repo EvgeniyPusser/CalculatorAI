d0764878896e330c58f851c8262c4220f9e6e749
4792411 <evgeniipusser88@gmail.com>
2024-06-06 12:34:12 +0300

    Initial commit

---
+++ src/ComplexNum.java
@@ -0,0 +1,17 @@
+public class ComplexNumber {
+    double RealPart;
+    double ImaginaryPart;
+
+    public ComplexNumber(double realPart, double imaginaryPart) {
+        RealPart = realPart;
+        ImaginaryPart = imaginaryPart;
+    }
+
+    public double getRealPart() {
+        return RealPart;
+    }
+
+    public double getImaginaryPart() {
+        return ImaginaryPart;
+    }
+}
---
+++ src/MultiplicationStrategy.java
@@ -0,0 +1,20 @@
+
+
+public class MultyplicationStrategy implements Strategy {
+    private ComplexNum a;
+    private ComplexNum b;
+
+    @Override
+    public void setNumbers(ComplexNum a, ComplexNum b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public ComplexNum calculate() {
+        double realPart = a.getRealPart() * b.getRealPart() - a.getImaginaryPart() * b.getImaginaryPart();
+        double imaginaryPart = a.getRealPart() * b.getImaginaryPart() + a.getImaginaryPart() * b.getRealPart();
+        return new ComplexNum (realPart, imaginaryPart);
+    }
+}
+
